01af237f133c5dedd9f18e4a43a5a55e6eec493f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-10-03 11:26:42 +0900

    :bulb:  Add example to `useToggle`

--- lib/useToggle.ts
+++ lib/useToggle.ts
@@ -2,12 +2,23 @@ import { useState } from 'react'
 
 /**
  * Hooks for toggler
- * @param initState - default:`false`
+ * @param initialState - default:`false`
+ * @returns A stateful value, and a toggle function to update it.
+ *
+ * @example
+ * ```ts
+ * const [state, toggle] = useToggle(false)
+ * state // false
+ * toggle()
+ * state // true
+ * toggle()
+ * state // false
+ * ```
  */
 const useToggle = (
-	initState: boolean | (() => boolean) = false
+	initialState: boolean | (() => boolean) = false
 ): [boolean, (value?: boolean) => boolean] => {
-	const [state, setState] = useState<boolean>(initState)
+	const [state, setState] = useState<boolean>(initialState)
 
 	const toggle = (value?: boolean): boolean => {
 		const _v = typeof value === 'boolean' ? value : !state
