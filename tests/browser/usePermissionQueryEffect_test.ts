6bc3c5ffed8343500b0ee494dcf57af79847a4ab
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-10-21 11:18:45 +0900

    :white_check_mark:  Remove unused test

--- tests/browser/usePermissionQueryEffect_test.ts
+++ tests/browser/usePermissionQueryEffect_test.ts
@@ -1,15 +1,5 @@
-import { renderHook } from '@testing-library/react-hooks'
-
 import { usePermissionQueryEffect } from '@/usePermissionQueryEffect'
 
 describe('usePermissionQueryEffect', () => {
   it('should be defined', () => expect(usePermissionQueryEffect).toBeDefined())
-
-  it('should', () => {
-    renderHook(() =>
-      usePermissionQueryEffect({
-        permission: 'camera'
-      })
-    )
-  })
 })
