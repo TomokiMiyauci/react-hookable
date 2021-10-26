fb4107197aba4d3756f840890c824f900ef46d5c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-10-26 18:51:46 +0900

    :recycle:  Add utility

--- lib/utils/index.ts
+++ lib/utils/index.ts
@@ -5,4 +5,17 @@ const isBrowser = typeof window !== 'undefined'
 
 const duplicate = <T>(value: T[]): T[] => Array.from(new Set(value))
 
-export { isBrowser, duplicate }
+const without = <T>(a: T[], b: T[]): T[] => {
+  const _a = duplicate(a)
+  const _b = duplicate(b)
+  return _a.reduce((acc, cur) => {
+    const hasSame = _b.some((__b) => __b === cur)
+
+    if (!hasSame) {
+      acc.push(cur)
+    }
+    return acc
+  }, [] as T[])
+}
+
+export { isBrowser, duplicate, without }
