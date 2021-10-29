8d669d5a86aec04f27a45ab6ea5778ee07f8651e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-10-29 21:48:05 +0900

    :memo: (docs) Add `useClickOutsideEffect` story

--- stories/useClickOutsideEffect/useClickOutsideEffect.story.tsx
+++ stories/useClickOutsideEffect/useClickOutsideEffect.story.tsx
@@ -1,13 +1,99 @@
-import { Box, Text, useToast } from '@chakra-ui/react'
+import { Box, Button, Text, useToast } from '@chakra-ui/react'
 import { useRef } from 'preact/hooks'
 
+import Portal from '@/components/Portal'
+import { useBoolean } from '@/useBoolean'
 import { useClickOutsideEffect } from '@/useClickOutsideEffect'
+import { useTransitionEffect } from '@/useTransitionEffect'
 import { flash } from '@story/shared/utils'
 import { argTypes } from '@story/useClickOutsideEffect/docs'
 
 import type { Meta, Story } from '@storybook/preact'
 
 import Docs from '@doc/useClickOutsideEffect.mdx'
+
+type DialogProps = {
+  show: boolean
+  children: JSX.Element
+  onClose?: () => void
+}
+
+const DialogComponent = ({
+  show,
+  children,
+  onClose
+}: DialogProps): JSX.Element => {
+  const target = useRef<HTMLDivElement>(null)
+  const dialog = useRef<HTMLDivElement>(null)
+
+  const { isShow } = useTransitionEffect(
+    {
+      target,
+      show,
+      enterFrom: 'md:opacity-0',
+      enter: 'transition duration-1000',
+      leaveTo: 'opacity-0',
+      leave: 'transition duration-500'
+    },
+    []
+  )
+
+  useTransitionEffect(
+    {
+      target: dialog,
+      show: isShow,
+      enterFrom: 'transform translate-y-full',
+      enter: 'transition duration-500',
+      leaveTo: 'transform translate-y-full',
+      leave: 'transition duration-500'
+    },
+    []
+  )
+
+  useClickOutsideEffect(
+    {
+      target: dialog,
+      onClickOutside: () => {
+        onClose?.()
+      }
+    },
+    [],
+    () => show
+  )
+
+  return (
+    <Portal>
+      <Box
+        inset="0"
+        cursor="pointer"
+        position="fixed"
+        p="20"
+        mx="auto"
+        className="md:p-40 backdrop-blur-md bg-opacity-10 backdrop-filter bg-gray-50"
+        ref={target}
+      >
+        <Box
+          position="absolute"
+          top="10%"
+          left="50%"
+          fontSize="4xl"
+          color="gray.300"
+          className="transform -translate-x-1/2 -translate-y-1/2"
+        >
+          outside
+        </Box>
+        <Box
+          fontSize="4xl"
+          color="gray.300"
+          className="border transition duration-300 cursor-auto h-full md:max-h-lg mx-auto max-w-2xl bg-gray-50 rounded-md shadow hover:shadow-md grid place-content-center"
+          ref={dialog}
+        >
+          {children}
+        </Box>
+      </Box>
+    </Portal>
+  )
+}
 
 const Template: Story = (): JSX.Element => {
   const toast = useToast({
@@ -65,6 +151,18 @@ const Template: Story = (): JSX.Element => {
 }
 
 export const Demo = Template.bind({})
+export const Dialog = (): JSX.Element => {
+  const [state, { on, off }] = useBoolean()
+
+  return (
+    <>
+      <DialogComponent onClose={off} show={state}>
+        <>inside</>
+      </DialogComponent>
+      <Button onClick={on}>SHOW</Button>
+    </>
+  )
+}
 
 export default {
   title: 'effect/useClickOutsideEffect',
